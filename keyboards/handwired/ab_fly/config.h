1a6918315109df5c5bd3a0bda9e75f03fd5bc618
Ankit Bhankharia <ankitbhankharia@Ankits-MacBook-Air.local>
2023-07-14 11:13:35 +0530

    version1 of ab_fly

---
+++ keyboards/handwired/ab_fly/config.h
@@ -0,0 +1,9 @@
+// Copyright 2023 Ankit Bhankharia (@cupcake08)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+// Options
+#define TAPPING_TERM 180
+#define PERMISSIVE_HOLD
+#define TAPPING_TERM_PER_KEY
